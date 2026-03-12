dfa3aac8cc29628c8d1cbd9db78a91d6cdb1156b
thiba93 <thibaultsene4@gmail.com>
2026-03-12 10:04:52 +0100

    Initial Commit

---
+++ CatalogueSwift/CatalogueSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogueSwiftApp.swift
+//  CatalogueSwift
+//
+//  Created by COURS on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CatalogueSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatalogueSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatalogueSwift
+//
+//  Created by COURS on 12/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
